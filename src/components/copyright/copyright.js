779f6a2d651371216426939b0e0a74fc090b2893
Jared <jared@tagtoo.org>
2019-10-01 22:39:38 +0800

    .

---
+++ src/components/copyright/copyright.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "components/copyright/copyright.scss";
+
+export default () => {
+  return <div className="copyright">© All Rights Reserved.</div>;
+};
---
+++ src/components/projects/projects.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "components/projects/projects.scss";
+
+import { Bookmark } from "components/utils/icons/icon";
+
+import { items_2017, items_2018, item_2019 } from "components/projects/projects-data";
+
+const renderList = (year, list) => {
+  return (
+    <div className="projects-row">
+      <div className="projects-year">{year}</div>
+      <div className="projects-item">
+        <ul>
+          {list.map((item, index) => (
+            <li key={index.toString()}>
+              <a target="_blank" rel="noopener noreferrer" href={item.url}>
+                {item.github}
+              </a>
+              <span>{item.tech_icon}</span>
+              <span>{item.text}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default () => {
+  return (
+    <>
+      <h3 className="item-title">
+        <Bookmark />
+        <span>Side Projects</span>
+      </h3>
+      {renderList(2019, item_2019)}
+      {renderList(2018, items_2018)}
+      {renderList(2017, items_2017)}
+    </>
+  );
+};
