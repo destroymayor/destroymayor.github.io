6089d2b02c48f31ce4783b281b4cdc9927eb4370
Jared <jared@tagtoo.org>
2019-10-02 10:41:12 +0800

    .

---
+++ build/precache-manifest.5159027b1eea4022e9d81ab92f025170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8378c5862f5cc19203036d2e622c1b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cf0f6bfb3ad137761c",
+    "url": "/static/css/main.23a66436.chunk.css"
+  },
+  {
+    "revision": "91114f2fbf978765f6ae",
+    "url": "/static/js/2.598d5771.chunk.js"
+  },
+  {
+    "revision": "87cf0f6bfb3ad137761c",
+    "url": "/static/js/main.28b302b2.chunk.js"
+  },
+  {
+    "revision": "fb1a82deb70a1ba250ab",
+    "url": "/static/js/runtime~main.8e14d2b1.js"
+  },
+  {
+    "revision": "fce3aaab6403de794abe978696074090",
+    "url": "/static/media/profile.fce3aaab.jpg"
+  }
+]);
