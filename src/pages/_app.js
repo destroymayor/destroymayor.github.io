a0e958b5bbb12f2ecd55d8a62273ecebe26c27ae
Jared <jared@tagtoo.com>
2021-02-14 23:01:59 +0800

    chore(_app): Modify google font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -20,7 +20,7 @@ const MyApp = ({ Component, pageProps }) => {
 
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Open+Sans&display=swap"
           rel="stylesheet"
         />
 
