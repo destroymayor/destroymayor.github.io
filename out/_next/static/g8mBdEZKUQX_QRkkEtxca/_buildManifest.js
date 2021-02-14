a139abf87248ba52c87981515288b048dbf478b7
Jared <jared@tagtoo.com>
2021-02-14 22:18:28 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/g8mBdEZKUQX_QRkkEtxca/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ab9ac0b95157990de35e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f9b3fd8e7dbb7332069.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
